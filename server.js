b86ba0912855ea7de7a720c3cfc74514c9105f45
syedmaaz99 <syedmaaziqbal99@gmail.com>
2023-04-30 03:04:53 -0700

    firstcommit

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.get('/about', (req, res) => {
+  res.send('Hello my name is maaz')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
