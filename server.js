4a9fa92b34a7c4235f2ac4fd09a3d0f16a5ce525
syedmaaz99 <syedmaaziqbal99@gmail.com>
2023-05-06 11:37:25 -0700

    Add new code

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/about', (req, res) => {
   res.send('Hello my name is maaz')
 })
 
+app.get('/contact', (req, res) => {
+  res.send('<h1>wellcome to contact page</h1>')
+})
+app.get('/home-page', (req, res) => {
+  res.send('<h1>wellcome to Home page</h1>')
+})
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
