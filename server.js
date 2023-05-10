5fc4fa4faac7bf1c7ed543d25e087f4675cec5ad
syedmaaz99 <116455089+syedmaaz99@users.noreply.github.com>
2023-05-10 14:09:55 -0700

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,64 @@
 const express = require('express')
-const app = express()
+const app = express() // call express 
 const port = 3000
 
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+
+
+//  mongoose initializastion
+
+const mongoose = require("mongoose")  // connet with atlas ==> online mongo db cloud
+/// connection
+ async function connectDb (){
+   try{
+    await mongoose.connect('mongodb+srv://syedmaaziqbal1:syedmaaz2045@cluster0.yhpatf4.mongodb.net/')
+    console.log('database connected successfully')
+  }
+  catch (error){
+
+    console.log(error)
+    process.exit(1)
+  }
+}
+
+connectDb() // calling
+
+const userSchema = new mongoose.Schema({   // data ka naqsha
+  username:String,
+  email:String,
+  phone:Number
 })
 
-app.get('/about', (req, res) => {
-  res.send('Hello my name is maaz')
+
+const User = mongoose.model('user',userSchema)
+
+app.post('/create',async(req,res)=>{
+  const {username,email,phone}=req.body
+  const user = new User ({
+    username,email,phone
+  })
+  await user.save();
+  res.status(200).json(user)
+
+})
+
+app.get('/getUser',async (req, res) => {
+  const user = await User.find()
+  res.status(200).json(user)
+})
+
+app.put('/update/:id', async (req, res) => {
+  const user = await User.findByIdAndUpdate(req.params.id,req.body,{new:true})
+  res.json(user)
+})
+app.delete('/delete/:id', async (req, res) => {
+  const user = await User.findByIdAndDelete(req.params.id,req.body,{new:true})
+  res.json(user)
 })
 
 app.get('/contact', (req, res) => {
-  res.send('<h1>wellcome to contact page</h1>')
+  res.send('<h1>wellcome to contact pag</h1>')
 })
 app.get('/home-page', (req, res) => {
   res.send('<h2>wellcome to Home page</h2>')
@@ -20,3 +66,6 @@ app.get('/home-page', (req, res) => {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
+
+
+// mongodb+srv://syedmaaziqbal1:<syedmaaz2045>@cluster0.yhpatf4.mongodb.net/
